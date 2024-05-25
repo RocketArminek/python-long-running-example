62bf235249a3df3e5ec82805746b29c88798a259
Arkadiusz Krakowiak <arkadiusz.k.e@gmail.com>
2024-05-25 14:28:52 +0200

    chore: update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import time
 
 def main():
-    print("Starting the job 30 seconds...")
-    time.sleep(30)
+    print("Starting the job 20 seconds...")
+    time.sleep(20)
     print("I am done!")
 
 if __name__ == "__main__":
