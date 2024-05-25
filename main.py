79a93bc6409c8e7d7118f54475768a3ed98464e3
Arkadiusz Krakowiak <arkadiusz.k.e@gmail.com>
2024-05-25 23:13:16 +0200

    chore: add print envs for tests

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import os
 import time
 
+def print_env_vars():
+    # Iterate through the environment variables and print them
+    for key, value in os.environ.items():
+        print(f"{key}: {value}")
+
 def main():
+    print_env_vars()
     print("Starting the job 20 seconds...")
     time.sleep(20)
     print("I am done!")
